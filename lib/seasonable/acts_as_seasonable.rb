e822aba4f1063973788f7d0caa1dc5c5679db935
Alejandro León <alejandro.leon@the-cocktail.com>
2015-07-29 11:46:09 +0200

    added class_name to relationship

--- lib/seasonable/acts_as_seasonable.rb
+++ lib/seasonable/acts_as_seasonable.rb
@@ -1,13 +1,13 @@
 module Seasonable
   module ActsAsSeasonable
     extend ActiveSupport::Concern
-    
+
     included do
-      has_many :season_constrains, as: :target
+      has_many :season_constrains, as: :target, class_name: 'Seasonable::SeasonConstrain'
       accepts_nested_attributes_for :season_constrains, allow_destroy: true
       scope :visible_today, -> {joins(:season_constrains).where("(:now BETWEEN season_constrains.starts_at AND season_constrains.ends_at) OR (season_constrains.ends_at IS NULL AND season_constrains.starts_at < :now)", now: Time.now)}
     end
-  
+
     def visible_today?
       right_now = Time.now
       season_constrains.any? do |season|
@@ -15,7 +15,7 @@ module Seasonable
       end || false
     end
   end
-  
+
   module SeasonsHelper
     def seasons_for seasonable, options = {}
       @template.render :partial=>"/seasonable/seasons_for", locals: {seasonable: seasonable, form: self}
